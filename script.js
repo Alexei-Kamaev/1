a0587b186c1ca24d0127a9e5e84c83d7344a4661
Alexei-Kamaev <alexei-kamaev@yandex.ru>
2023-09-07 13:48:56 +0300

    First lessons

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let nameProduct = 'Открытки';
+let quantityProduct = 35;
+const categoryProduct = 'Для праздника';
+let countProduct = 350;
+
+console.log('Наименование продукта: ', nameProduct);
+console.log('Общая сумма товара: ', quantityProduct*countProduct, 'рублей');
