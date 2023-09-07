9928f733af43e664b308dcdeb0d881ff4f1c5991
Alexei-Kamaev <alexei-kamaev@yandex.ru>
2023-09-07 13:55:01 +0300

    Two lessons

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 'use strict';
 
-let nameProduct = 'Открытки';
-let quantityProduct = 35;
-const categoryProduct = 'Для праздника';
-let countProduct = 350;
+{
+    let nameProduct = 'Открытки';
+    let quantityProduct = 35;
+    const categoryProduct = 'Для праздника';
+    let countProduct = 350;
 
-console.log('Наименование продукта: ', nameProduct);
-console.log('Общая сумма товара: ', quantityProduct*countProduct, 'рублей');
+    console.log('Наименование продукта: ', nameProduct);
+    console.log('Общая сумма товара: ', quantityProduct*countProduct, 'рублей');
+}
+
+{
+    let nameProduct = 'Кружки';
+    let quantityProduct = 25;
+    const categoryProduct = 'Для дома';
+    let countProduct = 450;
+
+    console.log('Наименование продукта: ', nameProduct);
+    console.log('Общая сумма товара: ', quantityProduct*countProduct, 'рублей');
+}
